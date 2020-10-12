5021791a7385ed0b73396dfffb649059278f3b27
JoyMbugua <wacumbugua@gmail.com>
2020-10-12 23:52:28 +0300

    add JS function

--- js/scripts.js
+++ js/scripts.js
@@ -24,16 +24,16 @@ $(document).ready(function() {
         alert("Thank you for contacting us!")
     })
 
-
-    function submit($form) {
-        $('#mc-embedded-subscribe').val('Sending...');
-        $.ajax({
-          type: $form.attr('method'),
-          url: $form.attr('action'),
-          data: $form.serialize(),
-          cache: false,
-          dataType: 'json',
-          contentType: 'application/json; charset=utf-8',
-          error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
-        }) }
 })
+
+function submit($form) {
+    $('#mc-embedded-subscribe').val('Sending...');
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
+    }) }
